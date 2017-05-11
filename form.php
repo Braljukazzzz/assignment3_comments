133ca515556e763c71944a8036c8d3d03d7b17e6
Braljukazzzz <bred2005@inbox.lv>
2017-05-12 00:12:51 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Thanks for your comment:<br>";
+echo "Здравствуйте,". $_POST['name1']."<br>";
+echo "Ваша оценка: ".$_POST['ocenka']."<br>";
+echo "Общий друг: ".$_POST['Friend']."<br>";
+echo "Message: ".$_POST['message']."<br>";
+
+
+
+?>
